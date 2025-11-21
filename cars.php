3f86a0d285bc3f6a47c4246a9dc7358988021e35
muhammad2812azhar <muhammad2812azhar@gmail.com>
2025-11-21 18:18:42 +0500

    a

--- cars.php
+++ cars.php
@@ -1,12 +1,11 @@
 <?php
-session_start();
-require_once 'apis/ApiHandler.php';
-$api = new ApiHandler();
+// Include global
+require_once 'global.php';
 
-$slug = $_GET['slug'] ?? null;
+// Default SEO fallback values
+$meta_title = '';
+$meta_desc  = '';
 
-
-include_once('header.php');
 
 if ($slug) {
 
@@ -19,6 +18,8 @@ if ($slug) {
     } catch (Exception $e) {
         echo "Error loading car details: " . $e->getMessage();
     }
+
+    include_once('header.php');
 
     $banner_image = "$imagePath/about/top-banner.webp";
     $banner_title = 'Cars';
@@ -190,6 +191,8 @@ try {
 } catch (Exception $e) {
     echo "Error loading car list: " . $e->getMessage();
 }
+
+include_once('header.php');
 
 $banner_image = "$imagePath/about/top-banner.webp";
 $banner_title = $messages['carsBannerHeading'];
