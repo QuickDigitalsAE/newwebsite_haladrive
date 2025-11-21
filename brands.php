244746326580cb2a27ac17ded6ac93811eef96af
muhammad2812azhar <muhammad2812azhar@gmail.com>
2025-11-21 12:00:14 +0500

    a

--- brands.php
+++ brands.php
@@ -40,7 +40,6 @@ try {
     }
     $banner_image = "$imagePath/about/top-banner.webp";
     $banner_title =  $brandData["brand"]["name_{$lang}"];
-    $banner_subtitle = "Top rated car rental in Dubai. Low prices, great deals, convenient pick-up, top-notch service!";
     include_once('banner.php');
     ?>
 
