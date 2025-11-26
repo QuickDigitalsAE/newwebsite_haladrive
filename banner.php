2f19f86e1060ee86f94c1b6339ae7328acdb6947
muhammad2812azhar <muhammad2812azhar@gmail.com>
2025-11-26 16:27:02 +0500

    a

--- banner.php
+++ banner.php
@@ -4,9 +4,7 @@
 ?>
 
 <section class="relative">
-    <img src="<?php echo $banner_image ?? 'images/default-banner.webp'; ?>" 
-         class="absolute w-full h-full object-cover banner_image inset-0" 
-         alt="">
+    <img src="<?php echo $banner_image ?? 'images/default-banner.webp'; ?>" class="absolute w-full h-full object-cover banner_image inset-0" alt="">
     <div class="w-[80%] max-[1024px]:w-[90%] mx-auto py-16 relative">
         <div class="text-white">
             <<?= $tag ?> class="font-bold text-[2rem] leading-[1]">
