7a1e52fe0017932064a7d3df0032773a59d880a8
muhammad2812azhar <muhammad2812azhar@gmail.com>
2025-12-04 17:58:20 +0500

    a

--- brands.php
+++ brands.php
@@ -229,7 +229,8 @@ include_once('banner.php');
                                     location.reload();
                                 " 
                                 rel="nofollow" 
-                                class="<?php ($_GET['sort'] == 'Crossover') ? 'bg-[#b8101f]' : 'bg-white' ?> border border-[#b8101f] py-2 px-4 col-span-2"
+                                class="<?= ($_GET['sort'] ?? '') == 'Crossover' ? 'bg-[#b8101f]' : 'bg-white' ?> border border-[#b8101f] py-2 px-4 col-span-2"
+
                             >
                                 <?= $messages['crossover'] ?>
                             </a>
